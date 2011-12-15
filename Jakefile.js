cbc85d822b4a8d36290727d22d2eadbeea55dbb7
Manuel van Rijn <manuel@manuelles.nl>
2011-12-15 15:36:56 +0100

    added package to build minified version

---
+++ Jakefile.js
@@ -0,0 +1,19 @@
+var version = 'v0.1';
+var header  =  "/* ==================================================================================\n";
+    header  += " * jquery.flickrPhotostream.js " + version + "\n";
+    header  += " * Description: Retrieve's a list of images from the public photostream of a user\n";
+    header  += " * Project: http://manuel.manuelles.nl/jquery-flickrPhotostream\n";
+    header  += " * Author: Manuel van Rijn <manuel@manuelles.nl> (http://manuel.manuelles.nl)\n";
+    header  += " * Released under the MIT License.\n";
+    header  += " * =============================================================================== */";
+
+var minify = require('jake-uglify').minify;
+
+task('default', ['jquery.flickrPhotostream.min.js']);
+
+desc('General-purpose build containing most features');
+minify({'jquery.flickrPhotostream.min.js': [
+    'src/jquery.flickrPhotostream.js',
+]},{
+  header: header
+});
